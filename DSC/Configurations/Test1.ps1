26124c2e707266b8dc209bb8e0a20f367671d542
Salvatore85 <salvatore.m@gmail.com>
2016-12-11 12:47:20 +0100

    create

---
+++ DSC/Configurations/Test1.ps1
@@ -0,0 +1,32 @@
+﻿configuration Test1 {
+
+    Import-DscResource -ModuleName PSDesiredStateConfiguration
+
+    Node "Test1" 
+    {
+    
+        File CreateDirectory
+        {
+        
+            Ensure = 'Present'
+            Type = 'Directory'
+            DestinationPath = 'C:\Management'
+            
+        }
+
+        File CreateTXT
+        {
+            DestinationPath = 'C:\Management\Test1.txt'
+            Type = 'File'
+            Ensure = 'Present'
+            Contents = 'Test succeeded'
+            DependsOn = '[File]CreateDirectory'
+        }
+
+        Log Test1
+        {
+            Message = 'Configuration Test1 applied succesfully'
+            DependsOn = '[File]CreateDirectory','[File]CreateTXT'
+        }
+    }
+}

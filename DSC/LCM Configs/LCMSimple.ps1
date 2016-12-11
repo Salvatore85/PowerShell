f4ae8d9f3a0b6409e3fd49048d8aa3305704bab2
Salvatore85 <salvatore.m@gmail.com>
2016-12-11 20:10:15 +0100

    Sample Files
    Testing purposes

---
+++ DSC/LCM Configs/LCMSimple.ps1
@@ -0,0 +1,28 @@
+﻿[DscLocalConfigurationManager()]
+Configuration LCMSimple
+{
+    Node localhost
+    {
+        Settings
+        {
+            RefreshMode = 'Pull'
+            RebootNodeIfNeeded = $true
+            ConfigurationMode = 'ApplyAndAutoCorrect'        
+            ActionAfterReboot = 'ContinueConfiguration'
+            AllowModuleOverwrite = $true
+        }
+
+        ConfigurationRepositoryWeb Pull
+        {
+            ServerURL = 'https://DSCPULL:8080/PSDSCPullServer.svc'
+            ConfigurationNames = 'Test1'
+            RegistrationKey = '0455c813-faa7-4b02-bcdf-da6047b36f81'
+        }
+
+        ReportServerWeb PullServer
+        {
+            ServerURL = 'https://DSCPULL:8080/PSDSCPullServer.svc'
+            RegistrationKey = '0455c813-faa7-4b02-bcdf-da6047b36f81'
+        }
+    }
+}
